dcc6b13225cdffcf333fa6b74fc1f99b672ff120
yixellet <yixwellet@gmail.com>
2023-05-28 21:10:25 +0400

    metadata query modified

--- controllers/metadata/index.js
+++ controllers/metadata/index.js
@@ -5,8 +5,53 @@ const { ParameterizedQuery } = require('pg-promise');
 function getAllMetadata(req, res) {
   const query = new ParameterizedQuery(
     {
-      text: `SELECT ST_AsGeoJSON(m.*, 'geom')
-             FROM metadata.metadata m;`,
+      text: `SELECT m.id,
+      json_build_object('id', sc.id, 'name', sc.name) AS scale, 
+      json_build_object('id', sf.id, 'name', sf.name) AS storageformat_ref, 
+      msc.name AS minscale, 
+      objectcreatedat, 
+      m.name, 
+      json_build_object('id', rs.id, 'name', rs.name) AS referencesystem_ref, 
+      rightholder_ref, 
+      creator_ref, 
+      json_build_object('id', scl.id, 'name', scl.name) AS secretclass_ref, 
+      extraregioninfo, 
+      comment, 
+      ac.name AS accesscondition_ref, 
+      guid, 
+      json_build_object('id', st.id, 'name', st.name) AS subtype_ref, 
+      incomingdoc_ref, 
+      outgoingdoc_ref, 
+      location_ref, 
+      nomenclature, 
+      objectquantity, 
+      maxareastatedate, 
+      areastatedate, 
+      objectchangedate, 
+      hs.name AS heightsystem_ref, 
+      r.json AS region_ref, 
+      json_build_object('id', g.id, 'name', g.name) AS group_ref, 
+      recordcreatedate, 
+      recordcreator_ref, 
+      location, 
+      rightholder, 
+      creator, 
+      incomingdoc, 
+      outgoingdoc,
+      ST_AsGeoJSON(m.*, 'geom') AS geom
+    FROM metadata.metadata m
+      LEFT JOIN metadata.scales sc ON m.scale = sc.id
+      LEFT JOIN metadata.access_conditions ac ON m.accesscondition_ref = ac.id
+      LEFT JOIN metadata.storage_formats sf ON m.storageformat_ref = sf.id
+      LEFT JOIN metadata.scales msc ON m.minscale = msc.id
+      LEFT JOIN metadata.reference_systems rs ON m.referencesystem_ref = rs.id
+      LEFT JOIN metadata.height_systems hs ON m.heightsystem_ref = hs.id
+      LEFT JOIN metadata.secret_classes scl ON m.secretclass_ref = scl.id
+      LEFT JOIN metadata.subtypes st ON m.subtype_ref = st.id
+      LEFT JOIN metadata.groups g ON m.group_ref = g.id
+      LEFT JOIN (SELECT r.id, json_build_object('region', r.name, 'district', rd.name) AS json
+            FROM metadata.regions r
+             JOIN metadata.regions rd ON r.parentregion_ref = rd.id) r ON m.region_ref = r.id;`,
     },
   );
   db.any(query)
